3edecf48ee42f0e30f2cf24975035cec66685c9d
sadapple9913 <jsc3441@gmail.com>
2023-05-15 15:48:19 +0900

    logo fi

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -49,7 +49,7 @@ function Nav({userObj}) {
 
     <nav className={`nav ${show && "nav__black"}`}>
     <img
-      src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
+      src={process.env.PUBLIC_URL + '/images/funfilx.png'}
       alt="netfilx logo"
       className="nav__logo"
       onClick={() => {
